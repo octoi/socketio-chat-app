a7cfae07898e0f290c302535a070c27b0a83b783
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-21 07:51:54 +0530

    get rooms function

--- server/mongo/handler.js
+++ server/mongo/handler.js
@@ -3,7 +3,7 @@ const { UserModel, ChatRoomModel } = require("./model");
 const signUp = (name, email, password) => {
     return new Promise((resolve, reject) => {
         const user = new UserModel({ name, email, password });
-        user.save().then(() => resolve()).catch(() => reject());
+        user.save().then(() => resolve()).catch(reject);
     });
 }
 
@@ -19,12 +19,19 @@ const login = (email, password) => {
 const createRoom = (name, description, host) => {
     return new Promise((resolve, reject) => {
         const room = new ChatRoomModel({ name, description, host, chats: [], users: [] });
-        room.save().then(roomData => resolve(roomData)).catch(() => reject());
+        room.save().then(roomData => resolve(roomData)).catch(reject);
+    });
+}
+
+const getAllRooms = () => {
+    return new Promise((resolve, reject) => {
+        ChatRoomModel.find().then(rooms => resolve(rooms)).catch(reject)
     });
 }
 
 module.exports = {
     signUp,
     login,
-    createRoom
+    createRoom,
+    getAllRooms
 }
