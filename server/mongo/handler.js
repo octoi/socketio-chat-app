e712ba8d2a56af4358b08f0e9925647297ae77ee
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-21 07:38:39 +0530

    login function

--- server/mongo/handler.js
+++ server/mongo/handler.js
@@ -3,19 +3,20 @@ const { UserModel, ChatRoomModel } = require("./model");
 const signUp = (name, email, password) => {
     return new Promise((resolve, reject) => {
         const user = new UserModel({ name, email, password });
-
-        user.save().then(() => {
-            console.log(`[+] REGISTERED ${email}`);
-            resolve();
-        }).catch(() => {
-            console.log(`[-] FAILED TO REGISTER ${email}`)
-            reject();
-        });
-
+        user.save().then(() => resolve()).catch(() => reject());
     });
 }
 
+const login = (email, password) => {
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({ email }).then(user => {
+            if (user.password === password) resolve(user);
+            reject();
+        }).catch(reject)
+    });
+}
 
 module.exports = {
-    signUp
+    signUp,
+    login
 }
