e5b5d1d13a02e6c6b2fecba463cc58bf5641a0c5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-21 07:38:45 +0530

    login route

--- server/router.js
+++ server/router.js
@@ -11,10 +11,15 @@ router.post('/user/signup', (req, res) => {
     mongo.signUp(name, email, password)
         .then(() => res.send('User created').status(200))
         .catch(() => res.send("Failed to create user").status(404))
-})
+});
 
 router.post('/user/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
 
-})
+    mongo.login(email, password)
+        .then(user => res.send(user).status(200))
+        .catch(() => res.send("Failed").status(404))
+});
 
 module.exports = router;
