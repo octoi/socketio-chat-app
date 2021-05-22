4a90af8f90765a0fe8cd52dfb0834d11e9376baa
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-22 11:32:12 +0530

    basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import ChakraWrap from './wrappers/ChakraWrap';
 import { Context } from './wrappers/Context';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
         <ChakraWrap>
             <Context>
-
+                <Router>
+                    <Switch>
+                        <Route exact path="*">
+                            <Redirect to="/" />
+                        </Route>
+                    </Switch>
+                </Router>
             </Context>
         </ChakraWrap>
     );
