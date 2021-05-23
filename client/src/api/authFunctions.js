811057118fbf95ace39ea58d572d62d217d23b10
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-23 14:40:49 +0530

    api calls

---
+++ client/src/api/authFunctions.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+export const loginUser = (userData) => {
+    return new Promise((resolve, reject) => {
+        const url = `${SERVER_URL}/user/login`;
+
+        axios.post(url, userData).then(res => {
+            if (res.data.status) resolve(res.data.message);
+            else reject();
+        }).catch(reject)
+    });
+}
+
+
+export const registerUser = (userData) => {
+    return new Promise((resolve, reject) => {
+        const url = `${SERVER_URL}/user/signup`;
+
+        axios.post(url, userData).then(res => {
+            if (res.data.status) resolve(res.data.message);
+            else reject();
+        }).catch(reject)
+    });
+}
