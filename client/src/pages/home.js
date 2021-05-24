57b7e47275174b5a3f77596a82c05c92d0691e02
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-24 11:45:41 +0530

    created rooms component and used it

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Rooms from '../components/home/Rooms';
 import { Container } from '@chakra-ui/react';
 
 export default function Home() {
     return (
         <Container marginTop="30px" maxW="container.xl">
-            Home content
+            <Rooms />
         </Container>
     )
 }
