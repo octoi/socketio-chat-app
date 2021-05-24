237f3369abac717ea5c3e384c2a8b69f18e50edd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-24 13:23:21 +0530

    displayed users

--- client/src/components/home/Room.jsx
+++ client/src/components/home/Room.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
-import { Button, Flex, Heading, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text, useToast } from '@chakra-ui/react';
+import { useDisclosure, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, ModalHeader, ModalFooter } from '@chakra-ui/react';
 
 export default function Room({ room }) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const toast = useToast();
+
+    room.users = [
+        { name: "sample", email: "test@test.com" },
+        { name: "sample", email: "test@test.com" },
+        { name: "sample", email: "test@test.com" },
+        { name: "sample", email: "test@test.com" },
+    ]
+
     return (
         <Flex background="#2D3748" width="100%" direction="column" mt={3} p={5} borderRadius={10}>
             <Heading size="md">{room.name}</Heading>
             <Text mt={3} color="gray.400">{room.description}</Text>
             <Flex width="100%" justifyContent="space-between" direction="row">
                 <Button mr={1} mt={5} width="100%" colorScheme="teal">Join Room</Button>
-                <Button ml={1} mt={5} width="100%">View Users</Button>
+                <Button ml={1} mt={5} width="100%" onClick={onOpen}>View Users</Button>
             </Flex>
+
+            <Modal
+                onClose={onClose}
+                isOpen={isOpen}
+                scrollBehavior="inside"
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Room Users</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Button
+                            onClick={() => toast({ isClosable: true, title: room.host.email, status: "info", position: "top-right", description: "Room host" })}
+                            mb={5}
+                            variant="solid"
+                            width="100%"
+                        >
+                            <Text>{room.host.name}</Text>
+                        </Button>
+                        {room.users.map((user, id) => (
+                            <Button
+                                onClick={() => toast({ isClosable: true, title: user.email, status: "info", position: "top-right", description: "Room user" })}
+                                mt={2}
+                                width="100%"
+                                key={id}
+                                variant="outline"
+                            >
+                                <Text>{user.name}</Text>
+                            </Button>
+                        ))}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={onClose}>Close</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Flex>
     )
 }
