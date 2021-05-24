4abcc6d3ce8a5bb8abc58fe9c0d298ff0323a8f1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-24 12:43:17 +0530

    fetching rooms and displaying

--- client/src/components/home/Rooms.jsx
+++ client/src/components/home/Rooms.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Text } from '@chakra-ui/react';
+import { fetchRooms } from '../../api/fetchRooms';
+import Room from './Room';
 
 export default function Rooms() {
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        fetchRooms().then(allRooms => {
+            setRooms(allRooms.reverse());
+        }).catch(() => alert("err"))
+    }, []);
+
     return (
         <section>
-            <Text fontSize="xl" color="grey">All Rooms</Text>
+            <Text fontSize="xl" color="grey" mb={10}>All Rooms</Text>
+            {rooms.map(room => <Room room={room} key={room?._id} />)}
         </section>
     )
 }
