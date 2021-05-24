8ef8ec7adcf5d197f9d5815833e05238ff932a0e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-24 16:39:49 +0530

    socket io server setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const socketIo = require("socket.io");
+const http = require("http");
 const cors = require("cors")
 const router = require("./router");
 
@@ -9,6 +11,8 @@ app.use(express.json())
 app.use(cors())
 app.use("/", router);
 
+const server = http.createServer(app);
+
 const mongodbURL = process.env.MONGODB || "mongodb://localhost:27017/chatapp";
 mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log(`[*] MONGO DB CONNECTED TO ${mongodbURL}`);
@@ -17,5 +21,11 @@ mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true }
     process.exit(0);
 })
 
+const io = socketIo(server);
+
+io.on("connection", socket => {
+    console.log(`${socket.id} connected`)
+})
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`[*] SERVER STARTED AT PORT ${port}`));
+server.listen(port, () => console.log(`[*] SERVER STARTED AT PORT ${port}`));
