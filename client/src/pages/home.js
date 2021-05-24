5fbd35bb7e05080b6ba098a8b1f73db5fdafe678
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-24 16:39:38 +0530

    socket io setup

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import socketIoClient from 'socket.io-client';
 import Rooms from '../components/home/Rooms';
+import useAppContext from '../hooks/useAppContext';
+import { useEffect } from 'react';
 import { Container } from '@chakra-ui/react';
 
 export default function Home() {
+    const { socket, setSocket } = useAppContext();
+
+    useEffect(() => {
+        if (socket) return;
+
+        const serverUrl = process.env.REACT_APP_SERVER_URL;
+        const sock = socketIoClient(serverUrl, { transports: ["websocket"] });
+        setSocket(sock);
+
+    }, [socket, setSocket]);
+
     return (
         <Container marginTop="30px" maxW="container.xl">
             <Rooms />
