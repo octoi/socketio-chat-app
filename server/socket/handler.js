00b0d26ea2ba65648222d49e2a505f4cdf6a8ab3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-24 17:00:24 +0530

    join room

--- server/socket/handler.js
+++ server/socket/handler.js
@@ -1,7 +1,17 @@
+const { joinRoom } = require("../mongo/handler");
+
 module.exports = socket => {
-    console.log(`[+] ${socket.id} client connected`)
+    console.log(`[+] ${socket.id} client connected`);
+
+    socket.on('joinRoom', (data, callback) => {
+        const { userData, roomId } = data;
+        socket.join(roomId);
+        joinRoom(roomId, userData)
+            .then(roomData => callback({ message: roomData, status: true }))
+            .catch(() => callback({ message: "No such room", status: false }))
+    });
 
     socket.on('disconnect', () => {
         console.log(`[-] ${socket.id} client disconnected`)
-    })
+    });
 }
