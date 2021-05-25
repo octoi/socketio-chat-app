5e57f6647a1fe82a9b73e1db093cc9ca17bbc6ab
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-25 07:51:23 +0530

    delete room

--- client/src/components/home/Room.jsx
+++ client/src/components/home/Room.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import useAppContext from '../../hooks/useAppContext';
+import { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button, Flex, Heading, Text } from '@chakra-ui/react';
-import { useDisclosure, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, ModalHeader, ModalFooter } from '@chakra-ui/react';
+import { useDisclosure, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, ModalHeader, ModalFooter, useToast } from '@chakra-ui/react';
+import { AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogCloseButton, AlertDialogBody, AlertDialogFooter } from '@chakra-ui/react';
+import { deleteRoom } from '../../api/deleteRoom';
 
 export default function Room({ room }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen: alertIsOpen, onOpen: alertOnOpen, onClose: alertOnClose } = useDisclosure()
+    const cancelRef = useRef()
     const history = useHistory();
+    const toast = useToast();
+
+    const { user } = useAppContext();
+    const amIHost = room.host.email === user.email;
 
     const joinRoom = () => {
         history.push(`/chat/${room._id}`)
+    }
+
+    const delRoom = () => {
+        deleteRoom(room._id, user.email).then(() => window.location.reload()).catch(() => {
+            toast({
+                title: "Failed to delete room 😓",
+                description: "It may be server error",
+                isClosable: true,
+                status: 'error'
+            });
+        })
     }
 
     return (
         <Flex background="#2D3748" width="100%" direction="column" mt={3} p={5} borderRadius={10}>
             <Heading size="md">{room.name}</Heading>
             <Text mt={3} color="gray.400">{room.description}</Text>
+            <Button mr={1} mt={5} mb={2} width="100%" colorScheme="teal" onClick={joinRoom}>Join Room</Button>
             <Flex width="100%" justifyContent="space-between" direction="row">
-                <Button mr={1} mt={5} width="100%" colorScheme="teal" onClick={joinRoom}>Join Room</Button>
-                <Button ml={1} mt={5} width="100%" onClick={onOpen}>View Host</Button>
+                <Button width="100%" onClick={onOpen}>View Host</Button>
+                {amIHost && <Button ml={1} width="100%" onClick={alertOnOpen}>Delete Room</Button>}
             </Flex>
 
             <Modal
@@ -40,6 +61,26 @@ export default function Room({ room }) {
                     </ModalFooter>
                 </ModalContent>
             </Modal>
+
+            <AlertDialog
+                motionPreset="slideInBottom"
+                leastDestructiveRef={cancelRef}
+                onClose={alertOnClose}
+                isOpen={alertIsOpen}
+                isCentered
+            >
+                <AlertDialogOverlay />
+
+                <AlertDialogContent>
+                    <AlertDialogHeader>Are you sure ?</AlertDialogHeader>
+                    <AlertDialogCloseButton />
+                    <AlertDialogBody>Are you sure you want to delete your room</AlertDialogBody>
+                    <AlertDialogFooter>
+                        <Button ref={cancelRef} onClick={alertOnClose}>No</Button>
+                        <Button colorScheme="red" ml={3} onClick={delRoom}>Yes</Button>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </Flex>
     )
 }
