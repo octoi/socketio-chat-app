119e91b32417eb30b9a740f1c9943c6f5f8dde34
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-25 06:45:56 +0530

    sending message

--- client/src/components/chat/ChatForm.jsx
+++ client/src/components/chat/ChatForm.jsx
@@ -1,8 +1,20 @@
 import { useState } from 'react';
 import { Button, Flex, Input } from '@chakra-ui/react';
+import useAppContext from '../../hooks/useAppContext';
 
-export default function ChatForm() {
+export default function ChatForm({ socket, roomId }) {
     const [msg, setMsg] = useState("");
+    const { user } = useAppContext();
+
+    const sendMessage = () => {
+        socket.emit("message", {
+            room: roomId,
+            chat: {
+                message: msg,
+                user
+            }
+        })
+    }
 
     return (
         <Flex mb={10}>
@@ -11,7 +23,7 @@ export default function ChatForm() {
                 value={msg}
                 onChange={(e) => setMsg(e.target.value)}
             />
-            <Button disabled={!msg} colorScheme="teal">SEND</Button>
+            <Button onClick={sendMessage} disabled={!msg} colorScheme="teal">SEND</Button>
         </Flex>
     )
 }
